d1cff0e43a8c3e053cb162f320bb44be46c84456
opsb <oliver@opsb.co.uk>
2010-11-25 17:32:28 +0000

    Removed dead code

--- lib/delayed_job_groups/mongoid_groups.rb
+++ lib/delayed_job_groups/mongoid_groups.rb
@@ -26,16 +26,3 @@ module Delayed
 
   end
 end
-
-__END__
-
-scope :in_unlocked_group, lambda{
-  delayed_jobs = self.table_name
-  unlocked_groups_select = "select distinct #{delayed_jobs}.lock_group from #{delayed_jobs} where #{delayed_jobs}.locked_by is not null"
-  where("#{delayed_jobs}.lock_group not in (#{unlocked_groups_select}) or #{delayed_jobs}.lock_group is null")
-}
-scope :orig_ready_to_run, scopes[:ready_to_run]
-scope :ready_to_run, lambda{ |*args|
-  orig_ready_to_run(*args).
-  in_unlocked_group
-}
